2e01bdf315d5a95aa413c4d5cace86cc22a288c5
monkhouse <henry.greenwold@gmail.com>
2017-12-10 00:52:00 +0200

    Added troll room

--- data/jigsaw.js
+++ data/jigsaw.js
@@ -5,10 +5,17 @@ module.exports = {
     rooms: [{
         name: 'Table Room',
         description: 'You are in a room that has a table. On that table is a jigsaw piece',
-        exits: [['n', 'Jigsaw Room']]
+        exits: [
+            ['n', 'Jigsaw Room'],
+            ['e', 'Troll Room']
+        ]
     },{
         name: 'Jigsaw Room',
         description: 'You are in a room with 499 pieces of a puzzle. One is missing',
         exits: [['s', 'Table Room']]
+    },{
+        name: 'Troll Room',
+        description: 'You are in a room with a giant troll who makes grunting noises.',
+        exits: [['w', 'Table Room']]
     }]
 };
